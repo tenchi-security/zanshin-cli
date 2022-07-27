df6f6023d259cf6a0782429a974f7fc0ee01f7e2
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2022-07-27 11:37:27 -0300

    Force utf8 encoding on setup.py; Remove typer cls to comply with version 0.6.1 and upgrade typer (#27)

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from pathlib import Path
 from os.path import join
+import re
 
 from setuptools import setup
 
-exec(open(join('zanshincli','version.py')).read())
+def get_version():
+    version_py = join('zanshincli','version.py')
+    regex = re.compile('__version__\s*=\s*[\'\"](?P<version>[\d\.]+)')    
+    return regex.finditer(open(version_py, 'r', encoding='utf8').read()).__next__().group('version')
 
 setup(
     name='zanshincli',
     description='Python command line utility to the Tenchi Security Zanshin API v1',
-    long_description=Path('README.rst').read_text(),
+    long_description=Path('README.rst').read_text(encoding="utf8"),
     author='Tenchi Security',
     author_email='contact@tenchisecurity.com',
-    version=__version__,
+    version=get_version(),
     url='https://github.com/tenchi-security/zanshin-cli',
     license='Apache Software License',
-    install_requires=['zanshinsdk==1.2.5', 'typer==0.3.2', 'prettytable==2.5.0', 'boto3~=1.21.24', 'boto3_type_annotations~=0.3.1'],
+    install_requires=['zanshinsdk==1.2.7', 'typer==0.6.1', 'prettytable==2.5.0', 'boto3~=1.21.24', 'boto3_type_annotations~=0.3.1'],
     tests_require=['pytest>=6.2,<7'],
     setup_requires=['pytest-runner==6.0.0'],
     packages=['zanshincli'],
