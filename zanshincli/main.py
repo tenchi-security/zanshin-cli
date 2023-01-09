11ffe34c6a08758c439d8fc6ccbf9e715dc0ee2e
Isabella Fagioli <ifagioli@tenchisecurity.com>
2023-01-09 14:03:58 -0300

    add feat to list the scan targets

--- zanshincli/main.py
+++ zanshincli/main.py
@@ -998,6 +998,25 @@ def organization_scan_target_scan_get(
     client = Client(profile=global_options['profile'])
     dump_json(client.get_organization_scan_target_scan(organization_id, scan_target_id, scan_id))
 
+###################################################
+# Scan Target Groups App
+###################################################
+
+scan_target_group_app = typer.Typer()
+organization_app.add_typer(scan_target_group_app, name="scan-target-groups",
+                   help="Operations on organizations scan target groups the API key owner has direct access to")
+
+
+@scan_target_group_app.command(name='scan-targets')
+def scan_target_groups_scan_targets(
+        organization_id: UUID = typer.Argument(..., help="UUID of the organization"),
+        scan_target_group_id: UUID = typer.Argument(..., help="UUID of the scan target group")    
+):
+    """
+    Get all scan targets from a specific scan target group.
+    """
+    client = Client(profile=global_options['profile'])
+    output_iterable(client.iter_scan_targets_from_group(organization_id, scan_target_group_id))
 
 ###################################################
 # Alert
