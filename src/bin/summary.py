b1a04177723b9b29685c02e0f2d6da6e86accac1
Eduardo Palhares <epalhares@tenchisecurity.com>
2025-01-20 11:08:42 -0300

    add summaries new granular share routes (#98)
    * add summaries new granular share routes
    
    * updating cursor on granular share routes
    
    * update fields description

--- src/bin/summary.py
+++ src/bin/summary.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 from uuid import UUID
 
 import typer
-from zanshinsdk import Client
+from zanshinsdk import AlertSeverity, Client, ScanTargetKind
 
 import src.config.sdk as sdk_config
 from src.lib.utils import dump_json
@@ -10,71 +10,61 @@ from src.lib.utils import dump_json
 app = typer.Typer()
 
 
-@app.command(name="alert")
-def summary_alert(
-    organization_id: UUID = typer.Argument(..., help="UUID of the organization"),
-    scan_target_id: Optional[List[UUID]] = typer.Option(
-        None,
-        help="Only summarize alerts from the specified scan targets, defaults to all",
-    ),
-):
-    """
-    Gets a summary of the current state of alerts for an organization, both in total and broken down by scan target.
-    """
-    client = Client(profile=sdk_config.profile)
-
-    dump_json(client.get_alert_summaries(organization_id, scan_target_id))
-
-
-@app.command(name="alert_following")
-def summary_alert_following(
+@app.command(name="scan_targets_following")
+def summary_scan_targets_following(
     organization_id: UUID = typer.Argument(..., help="UUID of the organization"),
     following_ids: Optional[List[UUID]] = typer.Option(
-        None,
-        help="Only summarize alerts from the specified following, defaults to all",
+        None, help="Only summarize scan targets from the specified following ids"
+    ),
+    following_tags: Optional[List[str]] = typer.Option(
+        None, help="Only summarize scan targets from the specified following tags"
+    ),
+    scan_target_kinds: Optional[List[ScanTargetKind]] = typer.Option(
+        None, help="Only summarize scan targets from the specified kinds"
+    ),
+    alert_severities: Optional[List[AlertSeverity]] = typer.Option(
+        None, help="Only summarize alerts with the specified severities"
+    ),
+    include_empty_following_tags: Optional[bool] = typer.Option(
+        None, help="Include alerts from scan targets without tags"
     ),
 ):
-    """
-    Gets a summary of the current state of alerts for followed organizations.
-    """
     client = Client(profile=sdk_config.profile)
+    dump_json(
+        client.get_scan_targets_following_summary(
+            organization_id=organization_id,
+            following_ids=following_ids,
+            following_tags=following_tags,
+            scan_target_kinds=scan_target_kinds,
+            alert_severities=alert_severities,
+            include_empty_following_tags=include_empty_following_tags,
+        )
+    )
 
-    dump_json(client.get_following_alert_summaries(organization_id, following_ids))
 
-
-@app.command(name="scan")
-def summary_scan(
+@app.command(name="scan_targets_detail")
+def summary_scan_targets_detail(
     organization_id: UUID = typer.Argument(..., help="UUID of the organization"),
     scan_target_ids: Optional[List[UUID]] = typer.Option(
-        None,
-        help="Only summarize alerts from the specified scan targets, defaults to all",
+        None, help="Only summarize scan targets from the specified scan target ids"
     ),
-    days: Optional[int] = typer.Option(
-        7, help="Number of days to go back in time in historical search"
+    scan_target_tags: Optional[List[str]] = typer.Option(
+        None, help="Only summarize scan targets from the specified scan target tags"
     ),
-):
-    """
-    Returns summaries of scan results over a period of time, summarizing number of alerts that changed states.
-    """
-    client = Client(profile=sdk_config.profile)
-
-    dump_json(client.get_scan_summaries(organization_id, scan_target_ids, days))
-
-
-@app.command(name="scan_following")
-def summary_scan_following(
-    organization_id: UUID = typer.Argument(..., help="UUID of the organization"),
-    following_ids: Optional[List[UUID]] = typer.Option(
-        None,
-        help="Only summarize alerts from the specified following, defaults to all",
+    scan_target_kinds: Optional[List[ScanTargetKind]] = typer.Option(
+        None, help="Only summarize scan targets from the specified kinds"
     ),
-    days: Optional[int] = typer.Option(
-        7, help="Number of days to go back in time in historical search"
+    alert_severities: Optional[List[AlertSeverity]] = typer.Option(
+        None, help="Only summarize alerts with the specified severities"
     ),
 ):
-    """
-    Returns summaries of scan results over a period of time, summarizing number of alerts that changed states.
-    """
     client = Client(profile=sdk_config.profile)
-
-    dump_json(client.get_scan_summaries(organization_id, following_ids, days))
+    dump_json(
+        client.get_scan_target_detail_summary(
+            organization_id=organization_id,
+            scan_target_ids=scan_target_ids,
+            scan_target_tags=scan_target_tags,
+            scan_target_kinds=scan_target_kinds,
+            alert_severities=alert_severities,
+        )
+    )
