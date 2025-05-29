90fd26fcf9d92c89353bc3d8fcf859ebc3f011a9
Pr0teus <felipeaesposito@gmail.com>
2025-05-29 19:19:40 -0300

    fix to pass black

--- src/lib/awsorgrun.py
+++ src/lib/awsorgrun.py
@@ -74,7 +74,10 @@ def awsorgrun(
                 )
             )
         if "Status" in account:
-            if account["Status"] == "SUSPENDED" or account["Status"] == "PENDING_CLOSURE":
+            if (
+                account["Status"] == "SUSPENDED"
+                or account["Status"] == "PENDING_CLOSURE"
+            ):
                 logger.info(
                     "skipping account {0:s} ({1:s}) because it is in {2:s} state".format(
                         account["Id"], account["Name"], account["Status"]
